a3847181b81876b23f1aa266b30db67883716ed5
beardcraft <sgtpunishment@gmail.com>
2014-11-13 16:30:08 +0000

    Achievements
    Started work on adding achievements

--- src/main/java/moze_intel/ssr/SSRCore.java
+++ src/main/java/moze_intel/ssr/SSRCore.java
@@ -11,6 +11,7 @@ import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import moze_intel.ssr.commands.KillCMD;
 import moze_intel.ssr.events.CreateShardEvent;
 import moze_intel.ssr.events.PlayerKillEntityEvent;
+import moze_intel.ssr.events.SSRAchievement;
 import moze_intel.ssr.events.ShardPickup;
 import moze_intel.ssr.gameObjs.ObjHandler;
 import moze_intel.ssr.utils.EntityMapper;
@@ -25,16 +26,6 @@ public class SSRCore {
 	public static final String NAME = "Soul Shards Reborn";
 	public static final String VERSION = "RC2.1";
 
-	/*
-	 * public static Achievement achievementCage;
-	 * public static Achievement achievementShard;
-	 * public static Achievement achievementTier1;
-	 * public static Achievement achievementTier2;
-	 * public static Achievement achievementTier3;
-	 * public static Achievement achievementTier4;
-	 * public static Achievement achievementTier5;
-	 */
-	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		SSRConfig.load(event);
@@ -43,43 +34,8 @@ public class SSRCore {
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		ObjHandler.registerObjs();
-		/*
-		 * achievementCage = new Achievement("achievement.createCage",
-		 * "createCage", -2, 0, ObjHandler.SOUL_CAGE, (Achievement) null)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * achievementShard = new Achievement("achievement.createShard",
-		 * "createShard", 0, 0, ObjHandler.SOUL_SHARD, (Achievement) null)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * achievementTier1 = new Achievement("achievement.tier1Shard",
-		 * "tier1Shard", 2, 0, new ItemStack(ObjHandler.SOUL_SHARD),
-		 * achievementShard).initIndependentStat().registerStat();
-		 * 
-		 * achievementTier2 = new Achievement("achievement.tier2Shard",
-		 * "tier2Shard", 4, 0, ObjHandler.SOUL_SHARD, achievementTier1)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * achievementTier3 = new Achievement("achievement.tier3Shard",
-		 * "tier3Shard", 6, 0, ObjHandler.SOUL_SHARD, achievementTier2)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * achievementTier4 = new Achievement("achievement.tier4Shard",
-		 * "tier4Shard", 8, 0, ObjHandler.SOUL_SHARD, achievementTier3)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * achievementTier5 = new Achievement("achievement.tier5Shard",
-		 * "tier5Shard", 10, 0, ObjHandler.SOUL_SHARD, achievementTier4)
-		 * .initIndependentStat().registerStat();
-		 * 
-		 * AchievementPage .registerAchievementPage(new AchievementPage(
-		 * "Soul Shards: Reborn", new Achievement[] { achievementCage,
-		 * achievementShard, achievementTier1, achievementTier2,
-		 * achievementTier3, achievementTier4, achievementTier5 }));
-		 * 
-		 * MinecraftForge.EVENT_BUS.register(new ShardPickup());
-		 */
-
+		SSRAchievement.Get();
+		MinecraftForge.EVENT_BUS.register(new ShardPickup());
 		MinecraftForge.EVENT_BUS.register(new PlayerKillEntityEvent());
 		MinecraftForge.EVENT_BUS.register(new CreateShardEvent());
 	}
---
+++ src/main/java/moze_intel/ssr/events/SSRAchievement.java
@@ -0,0 +1,54 @@
+package moze_intel.ssr.events;
+
+import moze_intel.ssr.gameObjs.ObjHandler;
+import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
+import net.minecraftforge.common.AchievementPage;
+
+public class SSRAchievement {
+	public static Achievement achievementCage;
+	public static Achievement achievementShard;
+	public static Achievement achievementTier1;
+	public static Achievement achievementTier2;
+	public static Achievement achievementTier3;
+	public static Achievement achievementTier4;
+	public static Achievement achievementTier5;
+	
+	public static void Get() {
+		achievementCage = new Achievement("achievement.createCage",
+				"createCage", -2, 0, ObjHandler.SOUL_CAGE, (Achievement) null)
+				.initIndependentStat().registerStat();
+
+		achievementShard = new Achievement("achievement.createShard",
+				"createShard", 0, 0, ObjHandler.SOUL_SHARD, (Achievement) null)
+				.initIndependentStat().registerStat();
+
+		achievementTier1 = new Achievement("achievement.tier1Shard",
+				"tier1Shard", 2, 0, new ItemStack(ObjHandler.SOUL_SHARD),
+				achievementShard).initIndependentStat().registerStat();
+
+		achievementTier2 = new Achievement("achievement.tier2Shard",
+				"tier2Shard", 4, 0, ObjHandler.SOUL_SHARD, achievementTier1)
+				.initIndependentStat().registerStat();
+
+		achievementTier3 = new Achievement("achievement.tier3Shard",
+				"tier3Shard", 6, 0, ObjHandler.SOUL_SHARD, achievementTier2)
+				.initIndependentStat().registerStat();
+
+		achievementTier4 = new Achievement("achievement.tier4Shard",
+				"tier4Shard", 8, 0, ObjHandler.SOUL_SHARD, achievementTier3)
+				.initIndependentStat().registerStat();
+
+		achievementTier5 = new Achievement("achievement.tier5Shard",
+				"tier5Shard", 10, 0, ObjHandler.SOUL_SHARD, achievementTier4)
+				.initIndependentStat().registerStat();
+
+		AchievementPage
+				.registerAchievementPage(new AchievementPage(
+						"Soul Shards: Reborn", new Achievement[] {
+								achievementCage, achievementShard,
+								achievementTier1, achievementTier2,
+								achievementTier3, achievementTier4,
+								achievementTier5 }));
+	}
+}
