fb02edabf4d98c7c584da2903d0a9d39b01d923f
UnstoppableN <nigelriem@hotmail.com>
2015-04-10 19:54:53 +0200

    Fixed harmless lightning not showing
    Fixed harmless lightning not showing

--- src/main/java/com/whammich/sstow/entity/EntityHarmlessLightningBolt.java
+++ src/main/java/com/whammich/sstow/entity/EntityHarmlessLightningBolt.java
@@ -4,118 +4,70 @@ import net.minecraft.entity.effect.EntityWeatherEffect;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
-public class EntityHarmlessLightningBolt extends EntityWeatherEffect{
+public class EntityHarmlessLightningBolt extends EntityLightningBolt {
+	private int lightningState;
+	public long boltVertex;
+	private int boltLivingTime;
 
-    /** Declares which state the lightning bolt is in. Whether it's in the air, hit the ground, etc. */
-    private int lightningState;
-    /** A random long that is used to change the vertex of the lightning rendered in RenderLightningBolt */
-    public long boltVertex;
-    /** Determines the time before the EntityLightningBolt is destroyed. It is a random integer decremented over time. */
-    private int boltLivingTime;
-    
-	public EntityHarmlessLightningBolt(World p_i1703_1_, double p_i1703_2_, double p_i1703_4_, double p_i1703_6_) {
-		super(p_i1703_1_);
-        this.setLocationAndAngles(p_i1703_2_, p_i1703_4_, p_i1703_6_, 0.0F, 0.0F);
-        this.lightningState = 2;
-        this.boltVertex = this.rand.nextLong();
-        this.boltLivingTime = this.rand.nextInt(3) + 1;
-//
-//        if (!p_i1703_1_.isRemote && p_i1703_1_.getGameRules().getGameRuleBooleanValue("doFireTick") && (p_i1703_1_.difficultySetting == EnumDifficulty.NORMAL || p_i1703_1_.difficultySetting == EnumDifficulty.HARD) && p_i1703_1_.doChunksNearChunkExist(MathHelper.floor_double(p_i1703_2_), MathHelper.floor_double(p_i1703_4_), MathHelper.floor_double(p_i1703_6_), 10))
-//        {
-//            int i = MathHelper.floor_double(p_i1703_2_);
-//            int j = MathHelper.floor_double(p_i1703_4_);
-//            int k = MathHelper.floor_double(p_i1703_6_);
-//
-//            if (p_i1703_1_.getBlock(i, j, k).getMaterial() == Material.air && Blocks.fire.canPlaceBlockAt(p_i1703_1_, i, j, k))
-//            {
-//                p_i1703_1_.setBlock(i, j, k, Blocks.fire);
-//            }
-//
-//            for (i = 0; i < 4; ++i)
-//            {
-//                j = MathHelper.floor_double(p_i1703_2_) + this.rand.nextInt(3) - 1;
-//                k = MathHelper.floor_double(p_i1703_4_) + this.rand.nextInt(3) - 1;
-//                int l = MathHelper.floor_double(p_i1703_6_) + this.rand.nextInt(3) - 1;
-//
-//                if (p_i1703_1_.getBlock(j, k, l).getMaterial() == Material.air && Blocks.fire.canPlaceBlockAt(p_i1703_1_, j, k, l))
-//                {
-//                    p_i1703_1_.setBlock(j, k, l, Blocks.fire);
-//                }
-//            }
-//        }
+	public EntityHarmlessLightningBolt(World par1World, double par2,
+			double par4, double par6) {
+		super(par1World, 0, Double.MAX_VALUE, 0);
+		this.setLocationAndAngles(par2, par4, par6, 0.0F, 0.0F);
+		this.lightningState = 2;
+		this.boltVertex = this.rand.nextLong();
+		this.boltLivingTime = this.rand.nextInt(3) + 1;
 	}
-	
-    /**
-     * Called to update the entity's position/logic.
-     */
-	public void onUpdate()
-    {
-	  super.onUpdate();
 
-      if (this.lightningState == 2)
-      {
-          this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "ambient.weather.thunder", 10000.0F, 0.8F + this.rand.nextFloat() * 0.2F);
-          this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ, "random.explode", 2.0F, 0.5F + this.rand.nextFloat() * 0.2F);
-      }
+	/**
+	 * Called to update the entity's position/logic.
+	 */
+	@Override
+	public void onUpdate() {
 
-      --this.lightningState;
+		if (this.lightningState == 2) {
+			this.worldObj.playSoundEffect(this.posX, this.posY, this.posZ,
+					"ambient.weather.thunder", 10000.0F,
+					0.8F + this.rand.nextFloat() * 0.2F);
+			this.worldObj
+					.playSoundEffect(this.posX, this.posY, this.posZ,
+							"random.explode", 2.0F,
+							0.5F + this.rand.nextFloat() * 0.2F);
+		}
 
-      if (this.lightningState < 0)
-      {
-          if (this.boltLivingTime == 0)
-          {
-              this.setDead();
-          }
-          else if (this.lightningState < -this.rand.nextInt(10))
-          {
-              --this.boltLivingTime;
-              this.lightningState = 1;
-              this.boltVertex = this.rand.nextLong();
+		--this.lightningState;
 
-          //    if (!this.worldObj.isRemote && this.worldObj.getGameRules().getGameRuleBooleanValue("doFireTick") && this.worldObj.doChunksNearChunkExist(MathHelper.floor_double(this.posX), MathHelper.floor_double(this.posY), MathHelper.floor_double(this.posZ), 10))
-        //      {
-      //            int i = MathHelper.floor_double(this.posX);
-    //              int j = MathHelper.floor_double(this.posY);
-  //                int k = MathHelper.floor_double(this.posZ);
-//
-              //    if (this.worldObj.getBlock(i, j, k).getMaterial() == Material.air && Blocks.fire.canPlaceBlockAt(this.worldObj, i, j, k))
-                 // {
-               //       this.worldObj.setBlock(i, j, k, Blocks.fire);
-             //     }
-           //   }
-          }
-      }
+		if (this.lightningState < 0) {
+			if (this.boltLivingTime == 0) {
+				this.setDead();
+			} else if (this.lightningState < -this.rand.nextInt(10)) {
+				--this.boltLivingTime;
+				this.lightningState = 1;
+				this.boltVertex = this.rand.nextLong();
+			}
+		}
 
-      if (this.lightningState >= 0)
-      {
-          if (this.worldObj.isRemote)
-          {
-              this.worldObj.lastLightningBolt = 2;
-          }
-          else
-          {
-	//        double d0 = 3.0D;
-	//        List list = this.worldObj.getEntitiesWithinAABBExcludingEntity(this, AxisAlignedBB.getBoundingBox(this.posX - d0, this.posY - d0, this.posZ - d0, this.posX + d0, this.posY + 6.0D + d0, this.posZ + d0));
-	//
-	//        for (int l = 0; l < list.size(); ++l)
-	//        {
-	//            Entity entity = (Entity)list.get(l);
-	//            if (!net.minecraftforge.event.ForgeEventFactory.onEntityStruckByLightning(entity, new EntityLightningBolt(this.worldObj,this.posX, this.posY, this.posZ)));
-	//                entity.onStruckByLightning(new EntityLightningBolt(this.worldObj,this.posX, this.posY, this.posZ));
-	//        }
-          }
-      }
-  }
+		if (this.lightningState >= 0) {
+			if (this.worldObj.isRemote) {
+				this.worldObj.lastLightningBolt = 2;
+			}
+		}
+	}
 
-  protected void entityInit() {}
+	@Override
+	protected void entityInit() {
+	}
 
-  /**
-   * (abstract) Protected helper method to read subclass entity data from NBT.
-   */
-  protected void readEntityFromNBT(NBTTagCompound p_70037_1_) {}
+	/**
+	 * (abstract) Protected helper method to read subclass entity data from NBT.
+	 */
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound par1NBTTagCompound) {
+	}
 
-  /**
-   * (abstract) Protected helper method to write subclass entity data to NBT.
-   */
-  protected void writeEntityToNBT(NBTTagCompound p_70014_1_) {}
+	/**
+	 * (abstract) Protected helper method to write subclass entity data to NBT.
+	 */
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound par1NBTTagCompound) {
+	}
 }
