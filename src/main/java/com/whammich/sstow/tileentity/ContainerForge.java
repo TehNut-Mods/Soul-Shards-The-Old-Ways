e3592a987c1f9b9174b4bfe5a09134312e27ac5a
UnstoppableN <nigelriem@hotmail.com>
2015-04-10 21:37:03 +0200

    Changed shift clicking functions
    Changed the way shift clicking functions so stacks will move to the right slot in the forge and back into the player inv.

--- src/main/java/com/whammich/sstow/tileentity/ContainerForge.java
+++ src/main/java/com/whammich/sstow/tileentity/ContainerForge.java
@@ -18,24 +18,28 @@ public class ContainerForge extends Container {
 	private int lastBurnTime;
 	private int lastItemBurnTime;
 
-	public ContainerForge(InventoryPlayer player, TileEntityForge tileEntityFurnace) {
+	public ContainerForge(InventoryPlayer player,
+			TileEntityForge tileEntityFurnace) {
 		this.tileFurnace = tileEntityFurnace;
 		this.addSlotToContainer(new Slot(tileEntityFurnace, 0, 56, 17));
 		this.addSlotToContainer(new Slot(tileEntityFurnace, 1, 56, 53));
-		this.addSlotToContainer(new SlotFurnace(player.player, tileEntityFurnace, 2, 116, 17));
-		this.addSlotToContainer(new SlotFurnace(player.player, tileEntityFurnace, 3, 116, 53));
-		
+		this.addSlotToContainer(new SlotFurnace(player.player,
+				tileEntityFurnace, 2, 116, 17));
+		this.addSlotToContainer(new SlotFurnace(player.player,
+				tileEntityFurnace, 3, 116, 53));
+
 		int i;
 		for (i = 0; i < 3; ++i) {
 			for (int j = 0; j < 9; ++j) {
-				this.addSlotToContainer(new Slot(player, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+				this.addSlotToContainer(new Slot(player, j + i * 9 + 9,
+						8 + j * 18, 84 + i * 18));
 			}
 		}
-		
+
 		for (i = 0; i < 9; ++i) {
 			this.addSlotToContainer(new Slot(player, i, 8 + i * 18, 142));
 		}
-		
+
 	}
 
 	public void addCraftingToCrafters(ICrafting craft) {
@@ -46,24 +50,27 @@ public class ContainerForge extends Container {
 	}
 
 	public void detectAndSendChanges() {
-		
+
 		super.detectAndSendChanges();
-		
+
 		for (int i = 0; i < this.crafters.size(); ++i) {
 			ICrafting craft = (ICrafting) this.crafters.get(i);
 			if (this.lastCookTime != this.tileFurnace.furnaceCookTime) {
-				craft.sendProgressBarUpdate(this, 0, this.tileFurnace.furnaceCookTime);
+				craft.sendProgressBarUpdate(this, 0,
+						this.tileFurnace.furnaceCookTime);
 			}
-			
+
 			if (this.lastBurnTime != this.tileFurnace.furnaceBurnTime) {
-				craft.sendProgressBarUpdate(this, 1, this.tileFurnace.furnaceBurnTime);
+				craft.sendProgressBarUpdate(this, 1,
+						this.tileFurnace.furnaceBurnTime);
 			}
-			
+
 			if (this.lastItemBurnTime != this.tileFurnace.currentBurnTime) {
-				craft.sendProgressBarUpdate(this, 2, this.tileFurnace.currentBurnTime);
+				craft.sendProgressBarUpdate(this, 2,
+						this.tileFurnace.currentBurnTime);
 			}
 		}
-		
+
 		this.lastBurnTime = this.tileFurnace.furnaceBurnTime;
 		this.lastCookTime = this.tileFurnace.furnaceCookTime;
 		this.lastItemBurnTime = this.tileFurnace.currentBurnTime;
@@ -71,19 +78,19 @@ public class ContainerForge extends Container {
 
 	@SideOnly(Side.CLIENT)
 	public void updateProgressBar(int par1, int par2) {
-		
+
 		if (par1 == 0) {
 			this.tileFurnace.furnaceCookTime = par2;
 		}
-		
+
 		if (par1 == 1) {
 			this.tileFurnace.furnaceBurnTime = par2;
 		}
-		
+
 		if (par1 == 2) {
 			this.tileFurnace.currentBurnTime = par2;
 		}
-		
+
 	}
 
 	@Override
@@ -91,68 +98,69 @@ public class ContainerForge extends Container {
 		return this.tileFurnace.isUseableByPlayer(player);
 	}
 
+	@Override
 	public ItemStack transferStackInSlot(EntityPlayer player, int par2) {
-		
+		System.out.println("SlotNumber: " + par2);
 		ItemStack itemstack = null;
-		
+
 		Slot slot = (Slot) this.inventorySlots.get(par2);
-		
+
 		if (slot != null && slot.getHasStack()) {
-			
+
 			ItemStack itemstack1 = slot.getStack();
 			itemstack = itemstack1.copy();
-			
+
 			if (par2 == 2) {
-				
-				if (!this.mergeItemStack(itemstack1, 3, 39, true)) {
+
+				if (!this.mergeItemStack(itemstack1, 4, 40, true)) {
 					return null;
 				}
-				
+
 				slot.onSlotChange(itemstack1, itemstack);
-				
+
 			} else if (par2 != 1 && par2 != 0) {
-				
+
 				if (SFRecipeHandler.smelting().getSmeltingResult(itemstack1) != null) {
-					
+
 					if (!this.mergeItemStack(itemstack1, 0, 1, false)) {
 						return null;
 					}
-					
+
 				} else if (TileEntityForge.isItemFuel(itemstack1)) {
-					
+
 					if (!this.mergeItemStack(itemstack1, 1, 2, false)) {
 						return null;
 					}
-					
+
 				} else if (par2 >= 3 && par2 < 30) {
-					if (!this.mergeItemStack(itemstack1, 30, 39, false)) {
+					if (!this.mergeItemStack(itemstack1, 31, 40, false)) {
 						return null;
 					}
 				} else if (par2 >= 30 && par2 < 39
-						
-						&& !this.mergeItemStack(itemstack1, 3, 30, false)) {
+
+				&& !this.mergeItemStack(itemstack1, 4, 31, false)) {
 					return null;
 				}
-				
-			} else if (!this.mergeItemStack(itemstack1, 3, 39, false)) {
+
+			} else if (!this.mergeItemStack(itemstack1, 4, 40, false)) {
 				return null;
 			}
-			
+
 			if (itemstack1.stackSize == 0) {
-				
+
 				slot.putStack((ItemStack) null);
-				
+
 			} else {
-				
+
 				slot.onSlotChanged();
-				
+
 			}
 			if (itemstack1.stackSize == itemstack.stackSize) {
 				return null;
 			}
-			
+
 			slot.onPickupFromSlot(player, itemstack1);
-			
+
 		}
 		return itemstack;
 	}
