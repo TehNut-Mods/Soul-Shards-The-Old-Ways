50d6f232dabc9048d9ed1ebac9d6db3af057a30d
Nick <speedynutty68@gmail.com>
2016-03-06 16:01:51 -0800

    Vile Sword can now be repaired with a Corrupted Ingot

--- src/main/java/com/whammich/sstow/item/ItemSoulSword.java
+++ src/main/java/com/whammich/sstow/item/ItemSoulSword.java
@@ -6,13 +6,16 @@ import com.whammich.sstow.SoulShardsTOW;
 import com.whammich.sstow.api.ISoulWeapon;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraftforge.common.util.EnumHelper;
 
 @ModItem(name = "ItemSoulSword")
 @Used
 public class ItemSoulSword extends ItemSword implements ISoulWeapon {
 
+    private static final ToolMaterial MATERIAL_SOUL = EnumHelper.addToolMaterial("SOUL", 2, 250, 6.0F, 2.0F, 14).setRepairItem(ItemMaterials.getStack(ItemMaterials.INGOT_CORRUPTED));
+
     public ItemSoulSword() {
-        super(ToolMaterial.IRON);
+        super(MATERIAL_SOUL);
 
         setCreativeTab(SoulShardsTOW.soulShardsTab);
         setUnlocalizedName(SoulShardsTOW.MODID + ".soulSword");
