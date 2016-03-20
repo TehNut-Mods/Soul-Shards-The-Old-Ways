fba74eab4bee8fdc7767bbcb7c27b50306fa3efd
Nick <speedynutty68@gmail.com>
2016-03-19 22:56:48 -0700

    Re-implement BloodMagic compat

--- src/main/java/com/whammich/sstow/compat/bloodmagic/CompatibilityBloodMagic.java
+++ src/main/java/com/whammich/sstow/compat/bloodmagic/CompatibilityBloodMagic.java
@@ -3,13 +3,14 @@ package com.whammich.sstow.compat.bloodmagic;
 import com.whammich.repack.tehnut.lib.iface.ICompatibility;
 import com.whammich.sstow.ConfigHandler;
 import com.whammich.sstow.compat.CompatibilityType;
+import net.minecraftforge.common.MinecraftForge;
 
 public class CompatibilityBloodMagic implements ICompatibility {
 
     @Override
     public void loadCompatibility(InitializationPhase phase) {
-//        if (phase == InitializationPhase.PRE_INIT)
-//            MinecraftForge.EVENT_BUS.register(new HandlerBloodMagic());
+        if (phase == InitializationPhase.PRE_INIT)
+            MinecraftForge.EVENT_BUS.register(new HandlerBloodMagic());
     }
 
     @Override
