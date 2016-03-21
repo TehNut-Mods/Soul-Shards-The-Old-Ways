3d5bd128a59aa71f40c1c1af48fd731e88df4ec7
Nick <speedynutty68@gmail.com>
2016-03-21 16:41:24 -0700

    Implement the Forge Version checker system

--- src/main/java/com/whammich/sstow/SoulShardsTOW.java
+++ src/main/java/com/whammich/sstow/SoulShardsTOW.java
@@ -30,12 +30,13 @@ import java.io.File;
 import java.util.Set;
 
 @Getter
-@Mod(modid = SoulShardsTOW.MODID, name = SoulShardsTOW.NAME, version = SoulShardsTOW.VERSION, guiFactory = "com.whammich.sstow.client.gui.GuiFactory")
+@Mod(modid = SoulShardsTOW.MODID, name = SoulShardsTOW.NAME, version = SoulShardsTOW.VERSION, updateJSON = SoulShardsTOW.JSON_CHECKER, guiFactory = "com.whammich.sstow.client.gui.GuiFactory")
 public class SoulShardsTOW {
 
     public static final String MODID = "soulshardstow";
     public static final String NAME = "SoulShardsTOW";
     public static final String VERSION = "@VERSION@";
+    public static final String JSON_CHECKER = "https://gist.githubusercontent.com/TehNut/e8db2be209d32d1ebbc3/raw/4c408d53b6fee1509705fe2e7ce6d827669d373d/VersionChecker-SSTOW.json";
 
     @Instance(MODID)
     public static SoulShardsTOW instance;
