4263ed21839837b3049121e9df4c7895ee35d2e1
Nick <speedynutty68@gmail.com>
2016-03-21 17:04:55 -0700

    Changelog + Version

--- src/main/java/com/whammich/sstow/api/package-info.java
+++ src/main/java/com/whammich/sstow/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "SoulShardsTOW", provides = "SoulShardsTOW|API", apiVersion = "0.1.3")
+@API(owner = "SoulShardsTOW", provides = "SoulShardsTOW|API", apiVersion = "0.1.4")
 package com.whammich.sstow.api;
 
 import net.minecraftforge.fml.common.API;
