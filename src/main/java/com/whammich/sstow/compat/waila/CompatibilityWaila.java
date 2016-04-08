51a5a4e5a6bdf2f59ae5a1095a4e2cce558adfbf
Nick <speedynutty68@gmail.com>
2016-04-07 21:51:26 -0700

    Re-implement Waila compat

--- src/main/java/com/whammich/sstow/compat/waila/CompatibilityWaila.java
+++ src/main/java/com/whammich/sstow/compat/waila/CompatibilityWaila.java
@@ -1,13 +1,14 @@
 package com.whammich.sstow.compat.waila;
 
+import net.minecraftforge.fml.common.event.FMLInterModComms;
 import tehnut.lib.iface.ICompatibility;
 
 public class CompatibilityWaila implements ICompatibility {
 
     @Override
     public void loadCompatibility(InitializationPhase phase) {
-//        if (phase == InitializationPhase.INIT)
-//            FMLInterModComms.sendMessage("Waila", "register", WailaCallbackHandler.class.getCanonicalName() + ".callbackRegister");
+        if (phase == InitializationPhase.INIT)
+            FMLInterModComms.sendMessage("Waila", "register", WailaCallbackHandler.class.getCanonicalName() + ".callbackRegister");
     }
 
     @Override
