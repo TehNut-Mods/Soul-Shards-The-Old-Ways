032457fdbbe8d419d9ebcd5abc0b6cc189c06d91
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-05-28 07:27:59 -0700

    Re-implement Waila compat

--- src/main/java/com/whammich/sstow/compat/waila/WailaCallbackHandler.java
+++ src/main/java/com/whammich/sstow/compat/waila/WailaCallbackHandler.java
@@ -1,20 +1,20 @@
-//package com.whammich.sstow.compat.waila;
-//
-//import com.whammich.sstow.SoulShardsTOW;
-//import com.whammich.sstow.block.BlockCage;
-//import com.whammich.sstow.compat.waila.provider.DataProviderCage;
-//import mcp.mobius.waila.api.IWailaRegistrar;
-//import tehnut.lib.annot.Used;
-//
-//@Used
-//public class WailaCallbackHandler {
-//
-//    public static final String CONFIG_OWNER = SoulShardsTOW.MODID + ".displayOwner";
-//
-//    @Used
-//    public static void callbackRegister(IWailaRegistrar registrar) {
-//        registrar.registerBodyProvider(new DataProviderCage(), BlockCage.class);
-//
-//        registrar.addConfig(SoulShardsTOW.MODID, CONFIG_OWNER, false);
-//    }
-//}
+package com.whammich.sstow.compat.waila;
+
+import com.whammich.sstow.SoulShardsTOW;
+import com.whammich.sstow.block.BlockCage;
+import com.whammich.sstow.compat.waila.provider.DataProviderCage;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import tehnut.lib.annot.Used;
+
+@Used
+public class WailaCallbackHandler {
+
+    public static final String CONFIG_OWNER = SoulShardsTOW.MODID + ".displayOwner";
+
+    @Used
+    public static void callbackRegister(IWailaRegistrar registrar) {
+        registrar.registerBodyProvider(new DataProviderCage(), BlockCage.class);
+
+        registrar.addConfig(SoulShardsTOW.MODID, CONFIG_OWNER, false);
+    }
+}
