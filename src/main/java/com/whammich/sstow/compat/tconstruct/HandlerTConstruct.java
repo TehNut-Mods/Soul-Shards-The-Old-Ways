c231e45dca1106a5e471d9347f28f7ce10d38ee9
Nicholas Ignoffo <speedynutty68@gmail.com>
2016-06-15 15:59:56 -0700

    Temporarily disable Modifier textures for release

--- src/main/java/com/whammich/sstow/compat/tconstruct/HandlerTConstruct.java
+++ src/main/java/com/whammich/sstow/compat/tconstruct/HandlerTConstruct.java
@@ -3,6 +3,8 @@ package com.whammich.sstow.compat.tconstruct;
 import com.whammich.sstow.SoulShardsTOW;
 import com.whammich.sstow.item.ItemMaterials;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.tools.TinkerTools;
 import slimeknights.tconstruct.tools.ToolClientProxy;
@@ -16,6 +18,7 @@ public class HandlerTConstruct {
         soulStealer.addItem(ItemMaterials.getStack(ItemMaterials.INGOT_CORRUPTED), 16, 1);
     }
 
+    @SideOnly(Side.CLIENT)
     public static void initRender() {
         ((ToolClientProxy) TinkerTools.proxy).registerModifierModel(soulStealer, new ResourceLocation(SoulShardsTOW.MODID, "models/item/modifiers/soulStealer"));
     }
--- src/main/java/com/whammich/sstow/compat/tconstruct/ModifierSoulStealer.java
+++ src/main/java/com/whammich/sstow/compat/tconstruct/ModifierSoulStealer.java
@@ -1,14 +1,20 @@
 package com.whammich.sstow.compat.tconstruct;
 
+import com.google.common.collect.ImmutableList;
 import com.whammich.sstow.util.Utils;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import slimeknights.tconstruct.library.Util;
+import slimeknights.tconstruct.library.materials.AbstractMaterialStats;
 import slimeknights.tconstruct.library.modifiers.ModifierAspect;
 import slimeknights.tconstruct.library.modifiers.ModifierNBT;
 import slimeknights.tconstruct.library.modifiers.ModifierTrait;
 import slimeknights.tconstruct.library.utils.TinkerUtil;
+
+import java.util.List;
 
 public class ModifierSoulStealer extends ModifierTrait {
 
@@ -30,5 +36,12 @@ public class ModifierSoulStealer extends ModifierTrait {
                     Utils.increaseShardKillCount(shardStack, data.level);
             }
         }
+    }
+
+    @Override
+    public List<String> getExtraInfo(ItemStack tool, NBTTagCompound modifierTag) {
+        String loc = String.format(LOC_Extra, getModifierIdentifier());
+        float amount = new ModifierNBT(modifierTag).level;
+        return ImmutableList.of(Util.translateFormatted(loc, AbstractMaterialStats.df.format(amount)));
     }
 }
